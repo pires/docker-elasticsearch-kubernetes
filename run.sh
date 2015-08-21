1448bea50791cdb7f86b93ce9a23a4407b209a4e
Paulo Pires <pjpires@gmail.com>
2015-08-21 11:33:15 +0100

    Disabled multicast discovery.

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ export CLUSTER_NAME=${CLUSTER_NAME:-elasticsearch-default}
 export NODE_MASTER=${NODE_MASTER:-true}
 export NODE_DATA=${NODE_DATA:-true}
 export HTTP_ENABLE=${HTTP_ENABLE:-true}
-export MULTICAST=${MULTICAST:-true}
+export MULTICAST=${MULTICAST:-false}
 
 export NAMESPACE=${NAMESPACE:-default}
 export DISCOVERY_SERVICE=${DISCOVERY_SERVICE:-elasticsearch-discovery}
