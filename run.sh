4460dba2172e2970e0478e8e466f123621807c74
Selva <k.n.selvakumar@gmail.com>
2016-04-21 00:41:58 +0530

    ENV variables for network.host and http.cors.enabled

--- run.sh
+++ run.sh
@@ -12,6 +12,8 @@ export CLUSTER_NAME=${CLUSTER_NAME:-elasticsearch-default}
 export NODE_MASTER=${NODE_MASTER:-true}
 export NODE_DATA=${NODE_DATA:-true}
 export HTTP_ENABLE=${HTTP_ENABLE:-true}
+export HTTP_CORS_ENABLE=${HTTP_CORS_ENABLE:-true}
+export NETWORK_HOST=${NETWORK_HOST:-_site_}
 export MULTICAST=${MULTICAST:-true}
 
 # Kubernetes stuff
